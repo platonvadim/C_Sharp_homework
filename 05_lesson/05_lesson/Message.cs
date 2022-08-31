a8316b77b0336010a78959ca5e069dfa88f17646
mgmoldova <vadim.platon97@gmail.com>
2022-08-31 17:27:33 +0300

    Lesson 5

---
+++ 05_lesson/05_lesson/Message.cs
@@ -0,0 +1,95 @@
+﻿/*
+Author: Vadim Platon 
+
+
+2. Разработать статический класс Message, содержащий следующие статические методы для обработки текста:
+а) Вывести только те слова сообщения, которые содержат не более n букв.
+б) Удалить из сообщения все слова, которые заканчиваются на заданный символ.
+в) Найти самое длинное слово сообщения.
+г) Сформировать строку с помощью StringBuilder из самых длинных слов сообщения.
+
+
+ */
+
+using System.Text;
+
+namespace _05_lesson
+{
+    internal static class Message
+    {
+        static public string text;
+
+        static Message()
+        {
+            text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, " +
+                "sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. " +
+                "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. " +
+                "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur." +
+                " Excepteur sint occaecat cupidatat perhvhenderit non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+        }
+
+        static public void PrintNLenghtWords(int len)
+        {
+            string[] words = text.Split(new Char[] { ' ', ',', '.', '-', '\n', '\t' });
+            
+            foreach (string word in words)
+            {
+                if (word == "")
+                    continue;
+                if (word.Length <= len)
+                    Console.Write(word + " ");
+            }
+        }
+
+        static public void DeleteAllWordByEndChar(char ch)
+        {
+            string[] words = text.Split(new Char[] { ' ', ',', '.', '-', '\n', '\t' });
+            foreach (string word in words)
+            {
+                if (word == "")
+                    continue;
+                if (word[word.Length - 1] == ch)
+                {
+                    Console.Write(word + " ");
+                    text.Replace(word, "");
+                }
+            }
+            
+        }
+
+        static public string FindLongesthWord()
+        {
+            string[] words = text.Split(new Char[] { ' ', ',', '.', '-', '\n', '\t' });
+            string maxWord = words[0];
+            int max = words[0].Length;
+
+            foreach (string word in words)
+            {
+                if (word.Length > max)
+                {
+                    max = word.Length;
+                    maxWord = word;
+                }
+            }
+          
+            return maxWord;
+        }
+
+
+        static public StringBuilder GetLongWordsString()
+        {
+            string[] words = text.Split(new Char[] { ' ', ',', '.', '-', '\n', '\t' });
+            StringBuilder result = new StringBuilder();
+            int max = Message.FindLongesthWord().Length;
+            foreach (string word in words)
+            {
+                if (word.Length == max)
+                {
+                    result.Append(word.ToLower() + " ");
+                }
+            }
+            return result;
+        }
+
+    }
+}
---
+++ 05_lesson/05_lesson/Program.cs
@@ -0,0 +1,38 @@
+﻿/*
+Author: Vadim Platon 
+
+
+2. Разработать статический класс Message, содержащий следующие статические методы для обработки текста:
+а) Вывести только те слова сообщения, которые содержат не более n букв.
+б) Удалить из сообщения все слова, которые заканчиваются на заданный символ.
+в) Найти самое длинное слово сообщения.
+г) Сформировать строку с помощью StringBuilder из самых длинных слов сообщения.
+
+ */
+using System.Text;
+using static _05_lesson.Message;
+namespace _05_lesson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.WriteLine($"Исходный текст \n{Message.text}");
+            Console.WriteLine("\n");
+            Console.WriteLine($"Вывод слов содержащих не более 5 символов:");
+            Message.PrintNLenghtWords(5);
+            Console.WriteLine("\n");
+            Console.WriteLine($"Удаляем все слова, которые заканчиваются на a:");
+            Message.DeleteAllWordByEndChar('a');
+            Console.WriteLine("\n");
+            Console.WriteLine($"Самое длинное слово: {Message.FindLongesthWord()}");
+            
+            Console.WriteLine("\n");
+            Console.WriteLine($"Список самых длинных слов: {Message.GetLongWordsString()}");
+            
+
+        }
+    }
+
+}
